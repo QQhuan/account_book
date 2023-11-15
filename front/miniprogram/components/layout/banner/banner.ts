bdca6c3e980f431d5b51fbc7ccb874571ef7d527
unknown <469558400@qq.com>
2023-11-15 10:23:01 +0800

    :sparkles:feat(front):banner

--- front/miniprogram/components/layout/banner/banner.ts
+++ front/miniprogram/components/layout/banner/banner.ts
@@ -3,15 +3,35 @@ Component({
   /**
    * 组件的属性列表
    */
-  properties: {},
+  properties: {
+    // year: {
+    //   type: Number,
+    //   value: 2023 // 给个默认值2023
+    // }
+  },
 
   /**
    * 组件的初始数据
    */
-  data: {},
+  data: {
+    selectYear: '', // 年份
+    selectMonth: '', // 月份
+    date: '2023年10月',
+    income: '', // 收入
+    outcome: '', // 指出
+    show: false
+  },
 
   /**
    * 组件的方法列表
    */
-  methods: {}
+  methods: {
+    showPopup() {
+      this.setData({ show: true })
+      wx.hideTabBar({})
+    },
+    onClose() {
+      this.setData({ show: false })
+    }
+  }
 })
