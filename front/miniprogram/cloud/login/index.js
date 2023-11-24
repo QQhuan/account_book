9bae2c9794c0738793e6a48489e093104ca0aa2f
QQhuan <469558400@qq.com>
2023-11-24 15:51:36 +0800

    :sparkles:feat(front):登录注册接口对接测试

---
+++ front/miniprogram/cloud/login/index.js
@@ -0,0 +1,34 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+var rp = require('request-promise')
+cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  let url = event.url;
+  let data = event.info;
+  // console.log(data);
+
+  if(url == null || url == undefined ){
+    return 'URL不存在'
+  }else{
+    return await rp({
+      url: url,
+      method: "POST",
+      body: data,
+      json: false,
+      headers: {
+        "content-Type": "text/plain",
+        // "content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        // 'User-Agent': 'Request-Promise'
+        // "token": event.token
+      },
+    })
+    .then(function (res) {
+      return res
+    })
+    .catch(function (err) {
+      return '请求失败'
+    });
+  }
+}
